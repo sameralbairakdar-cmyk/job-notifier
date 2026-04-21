996bda1adff5859ba8493afa35c09152b14f850a
sameralbairakdar-cmyk <sameralbairakdar@gmail.com>
2026-04-22 00:16:49 +0200

    Delete Downloads/job-notifier-complete directory

--- Downloads/job-notifier-complete/src/main/java/com/samer/jobs/model/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.samer.jobs.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-    boolean existsByUniqueKey(String uniqueKey);
-}
